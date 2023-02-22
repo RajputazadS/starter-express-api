59fd0733e63e1e8bf323bd082805bc814b8b2e8c
RajputazadS <126046681+RajputazadS@users.noreply.github.com>
2023-02-22 10:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const http  =require("http")
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const socketio= require("socket.io")
+const server = http.createServer(app);
+const port=3000
+// const io = socketio(server);
+
+const io = require('socket.io')(server, {
+  cors: {origin : '*'}
+});
+
+io.on('connection', client => {
+  client.on('message', data => { console.log(data);
+    io.emit('message', data)
+}
+  );
+  client.on('disconnect', () => {console.log('disconnect');});
+});
+server.listen(port,()=>{
+    console.log("port:-"+port);
+});
